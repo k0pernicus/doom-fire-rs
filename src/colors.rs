7a8f31e61c9dd46906d8f40094a9be5570e3587e
k0pernicus <antonin.carette@gmail.com>
2020-05-03 19:28:09 +0200

    Added full colors

--- src/colors.rs
+++ src/colors.rs
@@ -7,10 +7,19 @@ fn to_decimal(decimal: char, unit: char) -> u8 {
 
 fn hexa_str_to_decimal(hexa: &str) -> Option<(u8, u8, u8)> {
     let c = hexa.to_lowercase().chars().collect::<Vec<char>>();
-    return Some((to_decimal(c[0], c[1]), to_decimal(c[2], c[3]),to_decimal(c[4], c[5])));
+    return Some((
+        to_decimal(c[0], c[1]),
+        to_decimal(c[2], c[3]),
+        to_decimal(c[4], c[5]),
+    ));
 }
 
-const HEXA_COLORS: [&str; 2] = ["000000", "ffffff"];
+const HEXA_COLORS: [&str; 35] = [
+    "070707", "220606", "330D05", "4E0802", "5F1000", "701700", "811400", "9B1900", "AD2100",
+    "BD3400", "CE3C00", "D73A00", "F14000", "F14A00", "E75600", "E75F00", "DD6900", "DC7200",
+    "DB7B00", "DA8300", "D18400", "D08D00", "CF9500", "C49E00", "C49E00", "C3A600", "C3A600",
+    "C2AF00", "B8AF00", "B6B800", "B6B800", "CED05E", "DEDF97", "EFEFC2", "FFFFFF",
+];
 
 // Convert all hexadecimal colors to triple decimal (u8) colors at compile time
 lazy_static! {
@@ -19,10 +28,9 @@ lazy_static! {
         .enumerate()
         .map(|(counter, c)| (c, counter as u8))
         .collect();
-    
-    pub static ref COLORS: [(u8, u8, u8); 2] = {
-        let mut a = [(0u8, 0u8, 0u8); 2];
-        for i in 0..2 {
+    pub static ref COLORS: [(u8, u8, u8); 35] = {
+        let mut a = [(0u8, 0u8, 0u8); 35];
+        for i in 0..35 {
             a[i] = hexa_str_to_decimal(HEXA_COLORS[i]).unwrap();
         }
         a
